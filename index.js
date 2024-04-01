ccefec56cf4ea0c4f2a9f45d293142001d263779
Amene <layla_1380@yahoo.com>
2024-04-01 13:25:29 -0500

    + concatenate

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+let currentUser = 'luna'
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!'
+let shortGreeting = "Welcome, " + "l" + "!"
+excitedWelcomeMessage = 'WELCOME TO FLATBOOK, LUNA!'
+
+
+
